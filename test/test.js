7437b094b1ccd84d8ecfb97bc97f9b6d382ab52d
kgryte <kgryte@gmail.com>
2016-02-24 17:11:34 -0800

    Rename variable in test file

--- test/test.js
+++ test/test.js
@@ -2,18 +2,18 @@
 
 // MODULES //
 
-var test = require( 'tape' );
+var tape = require( 'tape' );
 var MAX_FLOAT64 = require( './../lib' );
 
 
 // TESTS //
 
-test( 'main export is a number', function test( t ) {
-	t.ok( typeof MAX_FLOAT64 === 'number', 'main export is a number' );
+tape( 'main export is a number', function test( t ) {
+	t.equal( typeof MAX_FLOAT64, 'number', 'main export is a number' );
 	t.end();
 });
 
-test( 'export is equal to the max double-precision float-point number', function test( t ) {
+tape( 'export is equal to the max double-precision float-point number', function test( t ) {
 	t.equal( MAX_FLOAT64, Math.pow(2,1023) * (2-Math.pow(2,-52)), 'equals max value' );
 	t.equal( MAX_FLOAT64, Number.MAX_VALUE, 'equals max value (Number object)' );
 	t.end();
